b0f31f3e840b06ef9f026016645d1b5147bad6ae
krishnanooka96 <krishna.nooka96@gmail.com>
2024-05-02 16:12:28 +0530

    Customers Rewards API:: for managing customers rewards data

--- source/RewardsService/rewardsservice/url_patterns.py
+++ source/RewardsService/rewardsservice/url_patterns.py
@@ -1,9 +1,13 @@
 from handlers.order_handler import OrderHandler
 from handlers.customer_handler import CustomerHandler
+from RewardsService.rewardsservice.handlers.customers_rewards_handler import (
+    CustomersRewardsHandler,
+)
 from handlers.rewards_handler import RewardsHandler
 
 url_patterns = [
     (r"/orders", OrderHandler),
+    (r"/customers", CustomersRewardsHandler),
     (r"/customers/([^/]+)", CustomerHandler),
     (r"/rewards", RewardsHandler),
 ]
